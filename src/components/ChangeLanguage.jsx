6c073dc7df06f78096df034e887a3104a623497c
sina <sinaderik7@gmail.com>
2024-02-08 23:34:57 +0330

    feat: changeLanguage component created

---
+++ src/components/ChangeLanguage.jsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useRef, useState } from 'react'
+import usFlag from '@assets/images/us.png'
+import faFlag from '@assets/images/fa.png'
+
+
+export default function ChangeLanguage() {
+
+  const [show, setShow] = useState(false)
+  const ref = useRef();
+  useEffect(() => {
+
+    function checkIfClickOutside(e){
+      if(show && ref.current && !ref.current.contains(e.target)){
+        setShow(false)
+      }
+    }
+    document.addEventListener("mousedown", checkIfClickOutside)
+    
+    return () => {
+      document.removeEventListener("mousedown", checkIfClickOutside)
+    }
+  }, [show])
+  return (
+    <div className='dropdown'>
+      <a className='nav-flag dropdown-toggle' onClick={() => setShow(true)}>
+        <img src={usFlag} alt="usFlag" />
+      </a>
+      <div ref={ref} className={`dropdown-menu dropdown-menu-end ${show ? "show" : undefined}`}>
+        <a className='dropdown-item fw-bolder' style={{ textAlign: "right" }}>
+          <img className='ms-2' src={faFlag} alt="faFlag" width="20" />
+          <span className='align-middle'>فارسی</span>
+        </a>
+        <a className='dropdown-item fw-bolder' style={{ textAlign: "right" }}>
+          <img className='ms-2' src={usFlag} alt="faFlag" width="20" />
+          <span className='align-middle'>English</span>
+        </a>
+      </div>
+    </div>
+  )
+}
--- src/layouts/identity-layout.jsx
+++ src/layouts/identity-layout.jsx
@@ -1,20 +1,24 @@
 import { Outlet } from "react-router-dom";
+import ChangeLanguage from "../components/ChangeLanguage";
 
 export default function IdentityLayout() {
-    return (
-        <div className='main d-flex justify-content-center w-100'>
-        <main className='content d-flex p-0'>
-          <div className='container d-flex flex-column'>
-            <div className='row h-100'>
-              <div className='col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100'>
-                <div className='d-table-cell align-middle'>
-                    <Outlet />
-                    {/* outlet component shows that children should be implemented here */}
-                </div>
+  return (
+    <div className='main d-flex justify-content-center w-100'>
+      <nav className="navbar shadow-sm">
+        <ChangeLanguage />
+      </nav>
+      <main className='content d-flex p-0'>
+        <div className='container d-flex flex-column'>
+          <div className='row h-100'>
+            <div className='col-sm-10 col-md-8 col-lg-6 mx-auto d-table h-100'>
+              <div className='d-table-cell align-middle'>
+                <Outlet />
+                {/* outlet component shows that children should be implemented here */}
               </div>
             </div>
           </div>
-        </main>
-      </div>
-)
+        </div>
+      </main>
+    </div>
+  )
 }
