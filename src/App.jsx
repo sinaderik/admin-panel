238f24937a3d60301d68ad1ce338037e71b03805
sina <sinaderik7@gmail.com>
2024-02-10 18:53:58 +0330

    feat: dark mode and light mode compeleted

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,26 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import Login from './features/identity/components/Login'
 import Register from './features/identity/components/register'
 import { RouterProvider } from 'react-router-dom'
 import router from './router'
 import "./core/i18n"
+import { useAppContext } from './contexts/app/AppContext'
 
 export default function App() {
+  const {theme}=useAppContext()
+  
+  useEffect(()=>{
+    const head=document.head
+    const link=document.createElement("link")
+    link.rel="stylesheet"
+    link.href=`/css/${theme}.css`
+    head.appendChild(link)
+
+    return ()=>{
+      head.removeChild(link)
+    }
+  },[theme])
+
   return (
     <>
       <RouterProvider router={router} />
