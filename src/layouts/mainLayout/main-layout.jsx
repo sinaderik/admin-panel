137040a24860b1952ab1932888bf1181b5e099f7
sina <sinaderik7@gmail.com>
2024-03-22 22:00:59 +0430

    feat: footer component created

--- src/layouts/mainLayout/main-layout.jsx
+++ src/layouts/mainLayout/main-layout.jsx
@@ -1,24 +1,14 @@
-import React, { useState } from 'react'
+import React from 'react'
 import { Outlet } from 'react-router-dom'
-import ChangeLanguage from '../../components/ChangeLanguage'
-import ChangeTheme from '../../components/ChangeTheme'
-import { useAppContext } from "../../contexts/app/AppContext";
-import { useTranslation } from "react-i18next";
-
 import Sidebar from './sidebar';
 import TopNav from './top-nav';
+import Footer from './footer';
 
 export default function MainLayout() {
 
-  const [collapseSideBar, setCollapseSideBar] = useState(false)
-  const { showSidebar } = useAppContext();
-  const { t } = useTranslation();
-
   return (
     <div className='wrapper' style={{ minHeight: "100vh" }}>
-      
-        <Sidebar />
-      
+      <Sidebar />
       <div className='main'>
         <TopNav />
         <main className='content'>
@@ -26,20 +16,7 @@ export default function MainLayout() {
             <Outlet />
           </div>
         </main>
-        <footer className='footer'>
-          <div className="container-fluid">
-            <div className="row">
-              <div className="col-12">
-                <p className="mb-0">
-                  © 2023 -{" "}
-                  <a href="index.html" className="text-muted">
-                    {t('classbon')}
-                  </a>
-                </p>
-              </div>
-            </div>
-          </div>
-        </footer>
+        <Footer />
       </div>
     </div>
   )
